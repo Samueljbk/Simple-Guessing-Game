d8238dd38cd7fccc80d621d296759ae52868c4da
Samueljbk <samjbkyle@gmail.com>
2024-11-14 18:45:25 +1300

    fixed while loop

--- src/game.py
+++ src/game.py
@@ -1,7 +1,7 @@
 import random
 
 
-def game():
+def main():
     print(f"Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the coreect number.\n")
 
     print(f"Please select the difficulty level:")
@@ -18,18 +18,21 @@ def game():
         difficulty_choice = "Hard"
         lives = 3
     
-    print(f"Great! You have selected the {difficulty_choice} difficulty level.\nLet's start the game!")
+    print(f"\nGreat! You have selected the {difficulty_choice} difficulty level.\nLet's start the game!\n")
     random_number = random.randint(1, 100)
+    print(random_number)
     attempts = 1
 
     while lives > 0:
         guess = int(input("Enter your guess: "))
         if guess > random_number:
-            print(f"\nIncorrect! The number is less than {guess}\n")
+            print(f"Incorrect! The number is less than {guess}\n")
         if guess < random_number:
-            print(f"\nIncorrect!: The number is greater than {guess}\n")
+            print(f"Incorrect!: The number is greater than {guess}\n")
         if guess == random_number:
             print(f"Congarulations! You guessed the number in {attempts}")
-    attempts += 1
-    lives -= lives
+            
+        attempts += 1
+        lives = lives - 1
     
+main()
